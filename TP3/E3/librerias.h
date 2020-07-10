85fad7b7d6b73cf0e4151a5148d6583c8de6770b
daignespola <daignespola@gmail.com>
2020-07-10 02:12:28 -0300

    Fix ejercicio 3

--- TP3/E3/librerias.h
+++ TP3/E3/librerias.h
@@ -21,10 +21,14 @@
 #include <stdio.h>
 #include <sys/ipc.h>   
 #include <sys/shm.h>   
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/unistd.h>
 #include <fcntl.h>     
 #include <semaphore.h> 
 #include <string.h>
 #include <signal.h>
+#include <time.h>
 
 typedef struct
 {
@@ -69,6 +73,8 @@ typedef struct
 #define ERROR_ARCHIVO   2
 #define NO_ENCONTRADO   3
 #define ERROR_KEY       4
+#define ERROR_CANT_PARAMETROS    5
+
 #define LISTA_VACIA     -1
 #define DNI_NO_ENCONTRADO -2
 #define ARCHIVO_SOCIOS  "Socios.txt"
@@ -78,21 +84,23 @@ typedef struct
 #define FIN_ARCHIVO_ASISTENCIAS  "FIN-ASISTENCIA"
 #define FIN_ARCHIVO_PAGOS  "FIN-PAGOS"
 
-int actualiza_pago(t_socio arraySocios[CANT_MAX_SOCIOS], long dni, t_fecha fecha){
-    for (int i = 0; i < CANT_MAX_SOCIOS; i++)
-    {
-        if(arraySocios[i].dni == dni){
-            if(strcmp(arraySocios[i].deporte,"Futbol")==0)
-                arraySocios[i].pago.montoPagado=1000;      
-            else if(strcmp(arraySocios[i].deporte,"Basquet")==0)
-                arraySocios[i].pago.montoPagado=1300; 
-            else if(strcmp(arraySocios[i].deporte,"Voley")==0)
-                arraySocios[i].pago.montoPagado=1200;   
-            else if(strcmp(arraySocios[i].deporte,"Natacion")==0)
-                arraySocios[i].pago.montoPagado=1800;          
-            if(fecha.dia < 11)
-                arraySocios[i].pago.montoPagado*=0.9; 
-            return OK;
+int actualiza_pago(t_socio arraySocios[CANT_MAX_SOCIOS], long dni, t_fecha fecha, int anioActual, int mesActual){
+    if (anioActual == fecha.anio && mesActual == fecha.mes) {
+        for (int i = 0; i < CANT_MAX_SOCIOS; i++)
+        {
+            if(arraySocios[i].dni == dni){
+                if(strcmp(arraySocios[i].deporte,"Futbol")==0)
+                    arraySocios[i].pago.montoPagado=1000;      
+                else if(strcmp(arraySocios[i].deporte,"Basquet")==0)
+                    arraySocios[i].pago.montoPagado=1300; 
+                else if(strcmp(arraySocios[i].deporte,"Voley")==0)
+                    arraySocios[i].pago.montoPagado=1200;   
+                else if(strcmp(arraySocios[i].deporte,"Natacion")==0)
+                    arraySocios[i].pago.montoPagado=1800;          
+                if(fecha.dia < 11)
+                    arraySocios[i].pago.montoPagado*=0.9; 
+                return OK;
+            }
         }
     }
     return -1;
@@ -108,7 +116,6 @@ int actualiza_asistencia(t_socio arraySocios[CANT_MAX_SOCIOS], long dni, char di
     }
     return -1;
 }
-
 
 #endif // LIBRERIAS_H_INCLUDED
 
